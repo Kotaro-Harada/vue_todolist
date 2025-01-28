6df7a9900aa7b28ec5357a078545c7d3f85f2885
Kotaro-Harada <ibsbeb360@yahoo.co.jp>
2025-01-28 23:16:14 +0900

    Your commit message

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,17 @@
 class Task < ApplicationRecord
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 }, allow_blank: true
+  validates :start_datetime, presence: true
+  validates :end_datetime, presence: true
+  validate :end_datetime_after_start_datetime
+
+  private
+
+  # カスタムバリデーション: 終了日時が開始日時より後かどうかを確認
+  def end_datetime_after_start_datetime
+    if start_datetime.present? && end_datetime.present? && end_datetime < start_datetime
+      errors.add(:end_datetime, "must be after the start datetime")
+    end
+  end
 end
